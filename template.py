231f1914351f236c48670494a94d047aa952d8df
balwantmaurya949 <prem.maurya949@gmail.com>
2024-04-09 00:20:26 +0530

    utils added

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "Indian_Coin_Detection"
+project_name = "cnnClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
